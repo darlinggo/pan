4e7c83d117ff51b235a3a0127e181e7184cbac29
Dan Worth <djworth@dramafever.com>
2014-01-16 07:32:41 -0500

    Hooking pan up with travis

--- genreadme.sh
+++ genreadme.sh
@@ -1,1 +1,2 @@
-godoc2md github.com/DramaFever/pan > README.md
+echo "[![Build Status](https://travis-ci.org/DramaFever/pan.png)](https://travis-ci.org/DramaFever/pan)" > README.md
+godoc2md github.com/DramaFever/pan >> README.md
