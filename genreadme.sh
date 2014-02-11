0142bfebcd062d98a5224d9b45232e732485125b
Paddy Foran <paddy@paddy.io>
2014-02-11 17:23:30 -0500

    Update genreadme.sh
    Point to the new Travis URL, not the DramaFever one.

--- genreadme.sh
+++ genreadme.sh
@@ -1,2 +1,2 @@
-echo "[![Build Status](https://travis-ci.org/DramaFever/pan.png)](https://travis-ci.org/DramaFever/pan)" > README.md
+echo "[![Build Status](https://travis-ci.org/secondbit/pan.png)](https://travis-ci.org/secondbit/pan)" > README.md
 godoc2md github.com/DramaFever/pan >> README.md
